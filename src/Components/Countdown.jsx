5735dbb35626588b19ea5ce874d956b4562075d7
jackcaldwellnichols <111219464+JackCaldwellNichols@users.noreply.github.com>
2023-01-17 13:11:38 +0100

    Add files via upload

---
+++ src/Components/Countdown.jsx
@@ -0,0 +1,23 @@
+import React, {useContext} from 'react'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+import {SettingsContext} from '../Context/SettingsContext.js'
+
+const Countdown = ({key, timer, animate, children}) => {
+    const {stopAnimate} = useContext(SettingsContext)
+  return (
+    <CountdownCircleTimer 
+                key={key} 
+                isPlaying={animate} 
+                duration={timer*60} 
+                colors={[ ['#FE6F68', 0.33],['#FE6F68', 0.33],['#FE6F68', 0.33]]} 
+                strokeWidth = {6}
+                size={220}
+                trailColor='#151932'
+                onComplete={() => {stopAnimate()}}
+    >
+        {children}
+    </CountdownCircleTimer>
+  )
+}
+
+export default Countdown
